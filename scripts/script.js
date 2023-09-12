3e35685cabbcaf34f059c545c14a4d3cf6c6c903
i-eat-rocks <143463845+i-eat-rocks@users.noreply.github.com>
2023-09-11 20:35:08 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+  $("#gameImg").mousedown(function () {
+    $("#gameImg").attr("src", "../assets/dancing_cat.gif");
+    console.log("position: ", $("#gameImg").position());
+    // checkImg();
+    $("#feedback").attr("display", "none");
+  });
+
+  $("#gameImg").draggable({
+    // axis: "x",
+    containment: "#gameField",
+  });
+
+  $("#sizeUp").click(function () {
+    $("#gameImg").height(($("#gameImg").height() * 1.15));
+  })
+
+  $("#sizeDown").click(function () {
+    $("#gameImg").height(($("#gameImg").height() * 0.85));
+  })
+});
